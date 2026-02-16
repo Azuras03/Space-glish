f76d5583e862d60bcf0fe3ab34da1b3a3b6dc680
nicolas03 <nicolas.russo@telecomnancy.eu>
2026-02-16 22:49:27 +0100

    Ajout d'assets (play button)

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready():
 	$PlayButton.pressed.connect(_on_play_pressed)
+	$QuitButton.pressed.connect(_quit_game)
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/CategoryMenu.tscn")
+
+func _quit_game():
+	get_tree().quit()
