5418ce688cc38befe29b58a373c0674470ff5f88
nicolas03 <nicolas.russo@telecomnancy.eu>
2026-03-19 09:54:33 +0100

    Ajout des questions custom et cleanup du code

--- CategoryMenu.gd
+++ CategoryMenu.gd
@@ -1,10 +1,51 @@
 extends Control
 
+signal file_selected_async(path: String)
+
 func _ready():
 	$VBoxContainer/ConjugationBtn.pressed.connect(_on_category_selected.bind("res://Config/conjugation.json"))
 	$VBoxContainer/PhrasalBtn.pressed.connect(_on_category_selected.bind("res://Config/phrasal_verbs.json"))
 	$VBoxContainer/MixedBtn.pressed.connect(_on_category_selected.bind("res://Config/questions.json"))
+	$VBoxContainer/ImportBtn.pressed.connect(_import_button_action)
 
 func _on_category_selected(file_path: String):
 	GameManager.load_questions(file_path)
 	get_tree().change_scene_to_file("res://Scenes/DifficultyMenu.tscn")
+
+func _import_button_action():
+	var file_path: String = await _import_file()
+	if (file_path == ""): # Aucun fichier n'a été sélectionné
+		return
+	GameManager.load_questions(file_path)
+	var tree = Engine.get_main_loop() as SceneTree
+
+	if tree:
+	# 2. On change la scène de manière différée (très important après un callback natif)
+		tree.call_deferred("change_scene_to_file", "res://Scenes/DifficultyMenu.tscn")
+	else:
+		print("Erreur critique : Impossible de trouver le SceneTree.")
+
+func _import_file():
+	# 1. On définit les types de fichiers qu'on accepte (ici txt et json)
+	var filtres = PackedStringArray(["*.txt, *.json ; Fichiers de texte et données"])
+	
+	DisplayServer.file_dialog_show(
+		"Importer des questions", 
+		"", 
+		"", 
+		false, 
+		DisplayServer.FILE_DIALOG_MODE_OPEN_FILE, 
+		filtres, 
+		_on_fichier_selectionne
+	)
+	
+	var resultat_chemin: String = await file_selected_async
+	return resultat_chemin
+	
+func _on_fichier_selectionne(status: bool, chemins_selectionnes: PackedStringArray, index_filtre: int):
+	# Si status est "true", c'est que l'utilisateur a cliqué sur "Ouvrir" (et non "Annuler")
+	if status and chemins_selectionnes.size() > 0:
+		var chemin_du_fichier = chemins_selectionnes[0]
+		file_selected_async.emit(chemin_du_fichier)
+	else:
+		file_selected_async.emit("")
--- DifficultyMenu.gd
+++ DifficultyMenu.gd
@@ -3,12 +3,11 @@ extends Control
 func _ready():
 	# Connecter les boutons manuellement ou via l'éditeur
 	# Ici je suppose que les nœuds s'appellent EasyBtn, NormalBtn, etc.
-	$VBoxContainer/EasyBtn.pressed.connect(_on_difficulty_selected.bind("easy"))
-	$VBoxContainer/NormalBtn.pressed.connect(_on_difficulty_selected.bind("normal"))
-	$VBoxContainer/HardBtn.pressed.connect(_on_difficulty_selected.bind("hard"))
-	$VBoxContainer/HardcoreBtn.pressed.connect(_on_difficulty_selected.bind("hardcore"))
-	$VBoxContainer/EndlessBtn.pressed.connect(_endless_mode)
-	$VBoxContainer/RapidBtn.pressed.connect(_rapid_game)
+	$LearnContainer/EasyBtn.pressed.connect(_on_difficulty_selected.bind("easy"))
+	$LearnContainer/HardBtn.pressed.connect(_on_difficulty_selected.bind("hard"))
+	$LearnContainer/HardcoreBtn.pressed.connect(_on_difficulty_selected.bind("hardcore"))
+	$SpecialContainer/EndlessBtn.pressed.connect(_endless_mode)
+	$SpecialContainer/RapidBtn.pressed.connect(_rapid_game)
 
 func _on_difficulty_selected(difficulty: String):
 	GameManager.set_difficulty(difficulty)
